e2254ed608643ae17278605e3a1aee6f0bbdf054
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-03-01 19:09:16 +0100

    Adding Straight Enemy
    Adding first version of enemy which simply goes to left

---
+++ nimuairy/Assets/EnemyHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float startHealth = 100f;
+    [SerializeField] float currenttHealth;
+
+
+    void Start()
+    {
+        currenttHealth = startHealth;
+    }
+
+    public void DealDamage(float damage)
+    {
+        currenttHealth -= damage;
+    }
+}
