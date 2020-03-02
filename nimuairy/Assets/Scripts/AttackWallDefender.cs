511d13f4d6722d60fae11e94312d5ed3ef7cbb4f
Paweł Zajglicz <31182014+pawelzajglicz@users.noreply.github.com>
2020-03-02 21:48:41 +0100

    Battle screen (#2)
    * Adding helpers scripts
    
    Scripts helpers to vector math
    
    * Modifying lightning wizard fast attack
    
    Fastening and facing lightning to target
    
    * Adding first version lightning wizard slow attack
    
    Adding first version lightning wizard slow attack - yet without deling damage
    
    * Adding dealing damage to lightning wizard slow attack
    
    Adding dealing damage to lightning wizard slow attack
    
    * Fixing bug with living with 0HP
    
    Fixing bug with living with 0HP
    
    * Refactoring attackPower
    
    Moving attackPower to specific AttackParameters script

--- nimuairy/Assets/Scripts/AttackWallDefender.cs
+++ nimuairy/Assets/Scripts/AttackWallDefender.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class AttackWallDefender : MonoBehaviour
 {
-    [SerializeField] public float attackPower = 10f;
     [SerializeField] public float speed = 1f;
     [SerializeField] public Vector2 target;
 
@@ -21,6 +20,10 @@ public class AttackWallDefender : MonoBehaviour
     public void SetTarget(Vector2 newTarget)
     {
         target = newTarget;
+        if (newTarget != Vector2.zero)
+        {
+            transform.up = HolisticMath.LookAt2D(new Coords(transform.up), new Coords(this.transform.position), new Coords(newTarget)).ToVector();
+        }
     }
 
 }
---
+++ nimuairy/Assets/Scripts/AttackWallDefenderContinuos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AttackParameters))]
+public class AttackWallDefenderContinuos : AttackWallDefender
+{
+    [SerializeField] float attackTimeRate = 0.4f;
+    private HashSet<Enemy> enemyToDealDamage;
+    private AttackParameters attackParameters;
+
+    private void Awake()
+    {
+        enemyToDealDamage = new HashSet<Enemy>();
+        attackParameters = GetComponent<AttackParameters>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            DealDamageToEnemy(enemy);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            StopDealDamageToEnemy(enemy);
+        }
+    }
+
+    private void StopDealDamageToEnemy(Enemy enemy)
+    {
+        if (enemyToDealDamage.Contains(enemy))
+        {
+            enemyToDealDamage.Remove(enemy);
+        }
+    }
+
+    private void DealDamageToEnemy(Enemy enemy)
+    {
+        Health enemyHealth = enemy.GetComponent<Health>();
+        if (enemyHealth != null)
+        {
+            
+            enemyToDealDamage.Add(enemy);
+            StartCoroutine(ProcessDealingDamageToEnemy(enemy));
+        }
+    }
+
+    IEnumerator ProcessDealingDamageToEnemy(Enemy enemy)
+    {
+        yield return new WaitForSeconds(attackTimeRate);
+
+        if (enemyToDealDamage.Contains(enemy))
+        {
+            Health enemyHealth = enemy.GetComponent<Health>();
+            enemyHealth.DealDamage(attackParameters.GetAttackPower());
+            StartCoroutine(ProcessDealingDamageToEnemy(enemy));
+        }
+    }
+}
