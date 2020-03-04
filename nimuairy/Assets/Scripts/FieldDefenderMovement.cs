122a537f62a2a8c781f7604e6d0e8eb8fec12ea0
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-03-04 18:58:28 +0100

    Adding attack and defence state to field defender
    Adding attack and defence state to field defender

--- nimuairy/Assets/Scripts/FieldDefenderMovement.cs
+++ nimuairy/Assets/Scripts/FieldDefenderMovement.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FieldDefenderMovement : MonoBehaviour
 {
-    [SerializeField] float SpeedHorizontal = 0;
-    [SerializeField] float SpeedVertical = 0;
-    [SerializeField] float MaxHorizontalSpeed = 10;
-    [SerializeField] float MaxVerticalSpeed = 8;
-    [SerializeField] float Acceleration = 10;
-    [SerializeField] float Deceleration = 10;
+    [SerializeField] float speedHorizontal = 0;
+    [SerializeField] float speedVertical = 0;
+    [SerializeField] float maxHorizontalSpeed = 10;
+    [SerializeField] float maxVerticalSpeed = 8;
+    [SerializeField] float acceleration = 10;
+    [SerializeField] float deceleration = 10;
+
+    [SerializeField] float startMaxHorizontalSpeed;
+    [SerializeField] float startMaxVerticalSpeed;
+    [SerializeField] float startAcceleration;
+    [SerializeField] float startDeceleration;
+
+    StateManager stateManager;
+    [SerializeField] State state;
 
     int facingRight = 1;
 
+    private void Start()
+    {
+        startAcceleration = acceleration;
+        startDeceleration = deceleration;
+        startMaxHorizontalSpeed = maxHorizontalSpeed;
+        startMaxVerticalSpeed = maxVerticalSpeed;
+
+        stateManager = GetComponent<StateManager>();
+        if (stateManager)
+        {
+            SetState(stateManager.getCurrentState());
+        }
+    }
+
+    public void SetState(State state)
+    {
+        this.state = state;
+
+        acceleration = startAcceleration * state.GetSpeedModifier();
+        deceleration = startDeceleration * state.GetSpeedModifier();
+        maxHorizontalSpeed = startMaxHorizontalSpeed * state.GetSpeedModifier();
+        maxVerticalSpeed = startMaxVerticalSpeed * state.GetSpeedModifier();
+    }
 
     void Update()
     {
@@ -24,53 +56,53 @@ public class FieldDefenderMovement : MonoBehaviour
         CalculatingHorizontalSpeed();
         CalculatingVerticalSpeed();
 
-        transform.position = new Vector2(transform.position.x + SpeedHorizontal * Time.deltaTime, transform.position.y + SpeedVertical * Time.deltaTime);
+        transform.position = new Vector2(transform.position.x + speedHorizontal * Time.deltaTime, transform.position.y + speedVertical * Time.deltaTime);
         HandleFacingDirection();
     }
 
     private void CalculatingHorizontalSpeed()
     {
-        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && (SpeedHorizontal > -MaxHorizontalSpeed))
-            SpeedHorizontal = SpeedHorizontal - Acceleration * Time.deltaTime;
-        else if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && (SpeedHorizontal < MaxHorizontalSpeed))
-            SpeedHorizontal = SpeedHorizontal + Acceleration * Time.deltaTime;
+        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && (speedHorizontal > -maxHorizontalSpeed))
+            speedHorizontal = speedHorizontal - acceleration * Time.deltaTime;
+        else if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && (speedHorizontal < maxHorizontalSpeed))
+            speedHorizontal = speedHorizontal + acceleration * Time.deltaTime;
         else
         {
-            if (SpeedHorizontal > Deceleration * Time.deltaTime)
-                SpeedHorizontal = SpeedHorizontal - Deceleration * Time.deltaTime;
-            else if (SpeedHorizontal < -Deceleration * Time.deltaTime)
-                SpeedHorizontal = SpeedHorizontal + Deceleration * Time.deltaTime;
+            if (speedHorizontal > deceleration * Time.deltaTime)
+                speedHorizontal = speedHorizontal - deceleration * Time.deltaTime;
+            else if (speedHorizontal < -deceleration * Time.deltaTime)
+                speedHorizontal = speedHorizontal + deceleration * Time.deltaTime;
             else
-                SpeedHorizontal = 0;
+                speedHorizontal = 0;
         }
     }
 
     private void CalculatingVerticalSpeed()
     {
-        if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) && (SpeedVertical > -MaxVerticalSpeed))
-            SpeedVertical = SpeedVertical - Acceleration * Time.deltaTime;
-        else if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && (SpeedVertical < MaxVerticalSpeed))
-            SpeedVertical = SpeedVertical + Acceleration * Time.deltaTime;
+        if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) && (speedVertical > -maxVerticalSpeed))
+            speedVertical = speedVertical - acceleration * Time.deltaTime;
+        else if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && (speedVertical < maxVerticalSpeed))
+            speedVertical = speedVertical + acceleration * Time.deltaTime;
         else
         {
-            if (SpeedVertical > Deceleration * Time.deltaTime)
-                SpeedVertical = SpeedVertical - Deceleration * Time.deltaTime;
-            else if (SpeedVertical < -Deceleration * Time.deltaTime)
-                SpeedVertical = SpeedVertical + Deceleration * Time.deltaTime;
+            if (speedVertical > deceleration * Time.deltaTime)
+                speedVertical = speedVertical - deceleration * Time.deltaTime;
+            else if (speedVertical < -deceleration * Time.deltaTime)
+                speedVertical = speedVertical + deceleration * Time.deltaTime;
             else
-                SpeedVertical = 0;
+                speedVertical = 0;
         }
     }
 
     private void HandleFacingDirection()
     {
 
-        if (SpeedHorizontal < 0 && Mathf.Abs(SpeedHorizontal) > 0.01)
+        if (speedHorizontal < 0 && Mathf.Abs(speedHorizontal) > 0.01)
         {
             transform.rotation = Quaternion.AngleAxis(180, Vector3.up);
             facingRight = -1;
         }
-        else if (SpeedHorizontal > 0 && Mathf.Abs(SpeedHorizontal) > 0.01)
+        else if (speedHorizontal > 0 && Mathf.Abs(speedHorizontal) > 0.01)
         {
             transform.rotation = Quaternion.AngleAxis(0, Vector3.up);
             facingRight = 1;
---
+++ nimuairy/Assets/Scripts/States/State.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour
+{
+    [SerializeField] public float attackPowerModifier = 1.2f;
+    [SerializeField] public float speedModifier = 1.3f;
+
+   
+    public float GetAttackPowerModifier()
+    {
+        return attackPowerModifier;
+    }
+
+    public float GetSpeedModifier()
+    {
+        return speedModifier;
+    }
+}
