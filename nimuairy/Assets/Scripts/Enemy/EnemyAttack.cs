fda65451fd5f8e4355ab42fb018f96bfb3d8fedc
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-03-05 21:13:09 +0100

    Giving enemy ability to attack
    Giving enemy ability to attack

---
+++ nimuairy/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] public float attackPower = 35f;
+    [SerializeField] public float lifeTime = 0.5f;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    public float GetAttackPower()
+    {
+        return attackPower;
+    }
+
+    public float GetLifeTime()
+    {
+        return lifeTime;
+    }
+}
---
+++ nimuairy/Assets/Scripts/Enemy/EnemyAttacking.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttacking : MonoBehaviour
+{
+    [SerializeField] EnemyAttack attack;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FieldDefenderMovement fieldDefender = collision.GetComponent<FieldDefenderMovement>();
+
+        if (fieldDefender)
+        {
+            AttackFieldDefender(fieldDefender);
+        }
+    }
+
+    private void AttackFieldDefender(FieldDefenderMovement fieldDefender)
+    {
+        InstantiateAttack();
+        DealDamage(fieldDefender);
+    }
+
+    private void InstantiateAttack()
+    {
+        EnemyAttack attackInstance = Instantiate(attack, transform.position, Quaternion.identity) as EnemyAttack;
+        attackInstance.transform.parent = transform;
+    }
+
+    private void DealDamage(FieldDefenderMovement fieldDefender)
+    {
+        Health fieldDefenderHealth = fieldDefender.GetComponent<Health>();
+        if (fieldDefenderHealth)
+        {
+            fieldDefenderHealth.DealDamage(attack.GetAttackPower());
+        }
+    }
+}
