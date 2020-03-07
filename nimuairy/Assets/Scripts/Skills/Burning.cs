ec555dd8add3e03fd8ac1e0742c9cc8a131dcc3d
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-03-07 09:40:29 +0100

    Making enemies burning
    Making enemies burning

--- nimuairy/Assets/Scripts/Skills/Burning.cs
+++ nimuairy/Assets/Scripts/Skills/Burning.cs
@@ -1,18 +1,74 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Burning : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public float lifeTime = 0.5f;
+    [SerializeField] public float burnDamage = 22f;
+    [SerializeField] public float burnInterval = 0.4f;
+    [SerializeField] public float timeSinceLastBurnDamage;
+    [SerializeField] public Burning burning;
+
+    public Enemy enemyToBurn;
+
+    private void Start()
     {
-        
+        Destroy(gameObject, lifeTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        timeSinceLastBurnDamage += Time.deltaTime;
+
+        if (timeSinceLastBurnDamage > burnInterval)
+        {
+            timeSinceLastBurnDamage = 0.0f;
+            DamageEnemy();
+        }
+    }
+
+    private void DamageEnemy()
+    {
+        Health enemyHealth = enemyToBurn.GetComponent<Health>();
+        if (enemyHealth)
+        {
+            enemyHealth.DealDamage(burnDamage);
+        }
+    }
+
+    internal void burn(Enemy enemy)
+    {
+        enemyToBurn = enemy;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy != null && IsNotBurning(enemy))
+        {
+            SetFireTo(enemy);
+        }
+    }
+
+    private void SetFireTo(Enemy enemy)
+    {
+        Burning burningInstance = Instantiate(burning, enemy.transform.position, Quaternion.identity) as Burning;
+        burning.burn(enemy);
+        burningInstance.transform.parent = enemy.transform;
+    }
+
+    private bool IsNotBurning(Enemy enemy)
+    {
+        foreach (Transform child in enemy.transform)
+        {
+            Burning enemyBurning = child.transform.gameObject.GetComponent<Burning>();
+            if (enemyBurning)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
