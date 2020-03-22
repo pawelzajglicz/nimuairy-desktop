a46ff24ff8158de628406059805de9873fd75f64
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-03-22 09:42:55 +0100

    Creating Time Mager

---
+++ nimuairy/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+
+    [SerializeField] float playerTimeFactor;
+    [SerializeField] FieldDefenderMovement fieldDefender;
+
+    
+    void Start()
+    {
+        fieldDefender = FindObjectOfType<FieldDefenderMovement>();
+    }
+    
+    void Update()
+    {
+        playerTimeFactor = fieldDefender.GetTimeFactor();
+    }
+
+    public void SetPlayerTimeFactor(float newFactor)
+    {
+        playerTimeFactor = newFactor;
+    }
+
+    public float GetPlayerTimeFactor()
+    {
+        return playerTimeFactor;
+    }
+}
