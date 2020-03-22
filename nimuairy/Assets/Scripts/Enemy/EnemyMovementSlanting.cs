f500b1d05df6bdf6efe196fc6c54fefd77b9b2a4
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-03-22 10:29:03 +0100

    Adding Enemies moving in slanting direction

---
+++ nimuairy/Assets/Scripts/Enemy/EnemyMovementSlanting.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementSlanting : EnemyMovement
+{
+
+    [SerializeField] float timeToWaitToRotate = 0.75f;
+    [SerializeField] float timeToChangeVerticalDirection = 5f;
+    [SerializeField] float timeElapsedSinceLastDirectionChange = 0f;
+    [SerializeField] int verticalDirection  = 1;
+
+    public BoxCollider2D collider;
+
+    private void Awake()
+    {
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Wall>())
+        {
+            arrivedAtWall = true;
+        }
+
+        if (collision.gameObject.GetComponent<WorldLimiter>())
+        {
+            verticalDirection *= -1;
+            timeElapsedSinceLastDirectionChange = 0f;
+        }
+    }
+
+    protected override void ProcessStandardMove()
+    {
+        HandleVerticalDirection();
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 direction = Vector2.left + Vector2.up * verticalDirection;
+        transform.Translate(direction * currentSpeed * Time.deltaTime);
+    }
+
+    private void HandleVerticalDirection()
+    {
+        float timeFactor = 1f;
+        if (enemyTimeManagerReacting.isReactingToFieldDefenderTimeFactor)
+        {
+            timeFactor = timeManager.GetPlayerTimeFactor();
+        }
+
+        timeElapsedSinceLastDirectionChange += Time.deltaTime * timeFactor;
+        if (timeElapsedSinceLastDirectionChange > timeToChangeVerticalDirection)
+        {
+            verticalDirection *= -1;
+            timeElapsedSinceLastDirectionChange = 0f;
+        }
+    }
+
+    public override void SetStandardMoveAllowed(bool newValue)
+    {
+        base.SetStandardMoveAllowed(newValue);
+        StartCoroutine(ProcessRotatingAfterAWhile());
+    }
+
+    public override void StartMove()
+    {
+        if (!isStandardMoveAllowed)
+        {
+            isStandardMoveAllowed = true;
+            StartCoroutine(ProcessRotatingAfterAWhile());
+        }
+    }
+
+    IEnumerator ProcessRotatingAfterAWhile()
+    {
+
+        yield return new WaitForSeconds(timeToWaitToRotate);
+
+        transform.rotation = Quaternion.AngleAxis(0, Vector3.up);
+    }
+
+}
