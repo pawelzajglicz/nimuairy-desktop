09cc83ed3cc9336f705e80f696e038c9ec79985a
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-03-23 20:13:32 +0100

    Adding Wall Defenders Slots

---
+++ nimuairy/Assets/Scripts/WallDefenderSlot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDefenderSlot : MonoBehaviour
+{
+    [SerializeField] WallDefender wallDefender;
+
+    private void Start()
+    {
+        wallDefender.transform.position = transform.position;
+    }
+}
