a21410fa00e70ae286689c803a525bd7872ae3ca
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-03-24 21:37:29 +0100

    Adding Gunner Wall Defender

---
+++ nimuairy/Assets/Scripts/WallDefender/TargetFinderGunner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFinderGunner: TargetFinder
+{
+    System.Random random;
+
+    private void Awake()
+    {
+        random = new System.Random();
+    }
+
+    public override Vector2 FindTargetPosition()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+        if (enemies.Length == 0)
+        {
+            return GetDefaultTarget();
+        }
+
+        Enemy randomEnemy = enemies[random.Next(0, enemies.Length)];
+
+        return randomEnemy.transform.position;
+    }
+
+    public override GameObject FindTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+        if (enemies.Length == 0)
+        {
+            return null;
+        }
+
+        Enemy randomEnemy = enemies[random.Next(0, enemies.Length)];
+
+        return randomEnemy.gameObject;
+    }
+
+ 
+}
---
+++ nimuairy/Assets/Scripts/WallDefender/WallDefenderGunner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDefenderGunner : WallDefender
+{
+    
+    protected override void InstantiateSlowAttack()
+    {
+
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+        if (enemies.Length == 0)
+        {
+            return;
+        }
+
+        Debug.Log(enemies.Length);
+        foreach (Enemy enemy in enemies)
+        {
+            WallDefenderAction fastAttackInstance = Instantiate(fastAction, transform.position, Quaternion.identity) as WallDefenderAction;
+            fastAttackInstance.SetTarget(enemy.transform.position);
+            Debug.Log("a");
+        }
+
+    }
+}
