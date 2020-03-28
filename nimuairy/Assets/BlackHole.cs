36c0c5c9929de32636d8cfab972484ccc850cee7
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-03-28 17:31:38 +0100

    Adding black hole Skill

---
+++ nimuairy/Assets/BlackHole.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour
+{
+
+    [SerializeField] public float duration = 1.25f;
+    [SerializeField] public float elapsedTime;
+    [SerializeField] public float pullingForceValue = 0.01f;
+
+
+    private void Start()
+    {
+        Destroy(gameObject, duration);
+    }
+
+    private void Update()
+    {
+        PullEnemies();
+    }
+
+    private void PullEnemies()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+        if (enemies.Length > 0)
+        {
+            foreach (Enemy enemy in enemies)
+            {
+                PullEnemy(enemy);
+            }
+        }
+
+        elapsedTime += (Time.deltaTime * Time.deltaTime);
+    }
+
+    private void PullEnemy(Enemy enemy)
+    {
+
+        Vector2 direction = (transform.position - enemy.transform.position).normalized;
+
+        if (direction == Vector2.zero)
+        {
+            return;
+        }
+        Debug.Log("aaaa");
+        enemy.transform.position += (Vector3)(direction * pullingForceValue);
+    }
+}
---
+++ nimuairy/Assets/SkillBlackHole.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillBlackHole : Skill
+{
+    public FieldDefenderMovement fieldDefenderMovement;
+    [SerializeField] public BlackHole blackHole;
+
+    private void Awake()
+    {
+        activationKey = KeyCode.F;
+    }
+
+    private void Start()
+    {
+        fieldDefenderMovement = FindObjectOfType<FieldDefenderMovement>();
+    }
+
+
+    protected override void ActivateSkill()
+    {
+        Instantiate(blackHole, fieldDefenderMovement.transform.position, Quaternion.identity);
+    }
+
+   
+}
