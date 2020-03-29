7145115c2183bcec1bfdbaf720d23a5dab31523e
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-03-29 17:10:21 +0200

    Adding Resource MAnager

---
+++ nimuairy/Assets/Scripts/ResourcesManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcesManager : MonoBehaviour
+{
+    [SerializeField] public int MagicCrystalsAmount;
+}
