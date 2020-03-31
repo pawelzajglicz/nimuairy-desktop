fbb3c3b864ad19cab78cdd28aa62e23d9b249f6e
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-03-31 21:58:48 +0200

    Adding Start Game button

---
+++ nimuairy/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ nimuairy/Assets/StartGameButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameButton : MonoBehaviour
+{
+
+    public void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FindObjectOfType<LevelManager>().StartGame();
+        }
+    }
+}
