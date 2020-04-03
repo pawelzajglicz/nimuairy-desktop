8cd3011a3a4b67705e5d16927d53c87ce6a81497
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-04-03 21:58:04 +0200

    Adding Param Upgrader prefab

--- nimuairy/Assets/ParamUpgrader.cs
+++ nimuairy/Assets/ParamUpgrader.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ParamUpgrader : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] protected TextMeshPro currentValueTMP;
+    [SerializeField] protected TextMeshPro nextValueTMP;
+    [SerializeField] protected TextMeshPro costTMP;
+
+    [SerializeField] public Param param;
+
+    private string startCurrentValueText;
+    private string startNextValueText;
+    private string startCostText;
+
+    [SerializeField] public int level = 1;
+    [SerializeField] public int startCurrentValue = 5;
+    [SerializeField] public int startCost = 2;
+
+    [SerializeField] public int currentValue;
+    [SerializeField] public int nextValue;
+    [SerializeField] public int cost;
+
+    [SerializeField] public float nextValueFactor = 0.05f;
+    [SerializeField] public int addingValue = 2;
+
+    [SerializeField] public float costFactor = 0.05f;
+    [SerializeField] public int addingCost = 2;
+
     void Start()
     {
-        
+        startCurrentValueText = currentValueTMP.text;
+        startNextValueText = nextValueTMP.text;
+        startCostText = costTMP.text;
+
+        UpdateNumbers();
+        UpdateTexts();
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateNumbers()
     {
-        
+        currentValue = (int)(level * nextValueFactor + addingValue);
+        nextValue = (int)((level + 1) * nextValueFactor + addingValue);
+        cost = (int)(level * costFactor + addingCost);
+    }
+
+    void UpdateTexts()
+    {
+        currentValueTMP.text = startCurrentValueText + currentValue;
+        nextValueTMP.text = startNextValueText + nextValue;
+        costTMP.text = startCostText + cost;
     }
 }
